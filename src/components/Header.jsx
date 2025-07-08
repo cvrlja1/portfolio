4255338e976c2e51e7b38e51f680c34d6106b468
Nikola Cvrlja <cvrlja.nikola@gmail.com>
2025-07-08 21:16:40 +0200

    fixed viewport

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 function Header({ setCurrentPage }) {
     return (
-        <div className="flex justify-start p-9 space-x-14 h-[250px] items-start">
+        <div className="flex flex-col sm:flex-row sm:justify-start p-6 sm:p-9 space-y-4 sm:space-y-0 sm:space-x-14 items-start">
             <button className="bttn" type="button" onClick={() => setCurrentPage('projects')}>Projects</button>
             <button className="bttn" type="button" onClick={() => setCurrentPage('contact')}>Contact</button>
             <button className="bttn" type="button" onClick={() => setCurrentPage('about')}>About me</button>
